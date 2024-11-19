1c9547958e70f6084dc6b4ceaaaf3b4e6864c39b
Miguel Robles <miguelroblesmv@gmail.com>
2024-11-19 13:15:21 +0000

    date_stg

--- models/staging/others/stg_others__dates.sql
+++ models/staging/others/stg_others__dates.sql
@@ -6,25 +6,25 @@
 
 WITH 
 
-src_dates AS (
+dates_table AS (
 
     {{ dbt_date.get_date_dimension(
-        start_date = "2000-01-01", 
+        start_date = "2019-01-01", 
         end_date = "2050-12-31") }}
 
 ),
 
-stg_staging__dates AS (
+stg_others__dates AS (
 
     select 
-        {{ dbt_utils.generate_surrogate_key(['year_number', 'month_of_year', 'day_of_month']) }} AS date_id,
-        date_day,
+        
+        date_day as date_id,
         --prior_date_day,
         --next_date_day,
         --prior_year_date_day,
         --prior_year_over_year_date_day,
         --day_of_week,
-        day_of_week_iso,
+        --day_of_week_iso,
         day_of_week_name,
         day_of_week_name_short,
         day_of_month,
@@ -34,14 +34,13 @@ stg_staging__dates AS (
         --prior_year_week_start_date,
         --prior_year_week_end_date,
         --week_of_year,
-        iso_week_start_date,
-        iso_week_end_date,
+        --iso_week_start_date,
+        --iso_week_end_date,
         --prior_year_iso_week_start_date,
         --prior_year_iso_week_end_date,
-        iso_week_of_year,
+        --iso_week_of_year,
         --prior_year_week_of_year,
         --prior_year_iso_week_of_year,
-        {{ dbt_utils.generate_surrogate_key(['month_of_year']) }} AS month_id,
         month_of_year,
         month_name,
         month_name_short,
@@ -52,11 +51,10 @@ stg_staging__dates AS (
         quarter_of_year,
         quarter_start_date,
         quarter_end_date,
-        {{ dbt_utils.generate_surrogate_key(['year_number']) }} AS year_id,
         year_number
         --year_start_date,
         --year_end_date
-    FROM src_dates
+    FROM dates_table
 )
 
-SELECT * FROM stg_staging__dates
+SELECT * FROM stg_others__dates
